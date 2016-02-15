8aa872d4d1e7b77084dac49cfa5a416c0b01a9de
Alex Ly <alexly@Alexs-MBP.home>
2016-02-14 23:51:41 -0500

    Tested burger model methods. Have to figure out routing.

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,14 @@ var connection = require('./connection.js');
 
 // TODO Create orm object with SQL insertion methods for exportation
 var orm = {
+  // Show all the burgers
+  showBurgers: function(table, callBack){
+    var q = 'SELECT * FROM ' + table;
+    connection.query(q, function(err, res){
+      if(err){ throw err }
+      callBack(res);
+    });
+  },
   // Add burger to main area
   addBurger: function(table, burger, callBack){
     var q = 'INSERT INTO ' + table + '(burger_name, devoured) VALUES (?,?);';
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,15 @@
 var orm = require('../config/orm.js');
 
 var tastyBurger = {
+  showMeTheBurgers: function(cb){
+    orm.showBurgers('burgers', function(res){
+      console.log(res);
+    });
+  },
   addTheBurger: function(burgerName, cb){
     orm.addBurger('burgers', burgerName, cb);
   }
 }
 
+tastyBurger.showMeTheBurgers();
 // TEST SUCCESS - tastyBurger.addTheBurger works
