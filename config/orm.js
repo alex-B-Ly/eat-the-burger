fbb8b5bdf1f71c72b5ef00d33674e01382c482f6
Alex Ly <alexly@Alexs-MBP.home>
2016-02-14 20:31:56 -0500

    Taking a break, examining logic of app.

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,3 @@
 var connection = require('./connection.js');
 
-connection.query('INSERT INTO burgers (burger_name, devoured) VALUES (?,?)', ['Big Mac', true], function(err, res){
-  if(err){ throw err }
-
-  console.log('Inserted a new burger');
-});
+// TODO Create orm object with SQL insertion methods for exportation
