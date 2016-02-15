021d8ac0de96bcd27c6fc1cfe27386cd3b55ad98
Alex Ly <alexly@Alexs-MBP.home>
2016-02-15 04:30:41 -0500

    Successfully tested add burger route with Beef Heaven.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -8,4 +8,15 @@ router.get('/', function(req, res){
   });
 });
 
+router.post('/add-burger', function(req, res){
+  // Burger method called, takes burger_name from name attr and callback that redirects user to get route above
+    // TODO replace 'Beef Heaven' burger with req.body.whatever_is_in_name_attribute
+  burger.addTheBurger('Beef Heaven', function(result){
+    console.log('Burger added to table');
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
+
+// TEST SUCCESS - home get route and add-burger work (tested with get method)
